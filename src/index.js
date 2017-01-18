d6c134e674c67c20b646e2c3b1a7108da8520dbf
Stephen Scott <me@suchipi.com>
2017-01-17 18:23:11 -0700

    extractFonts wip

--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
 // @flow
 import temp from 'temp';
+import pify from 'pify';
+import path from 'path';
+import mkdirp from 'mkdirp';
 import { parse } from './config';
 import type { InputConfig, Config } from './config';
+import exec from './exec';
 
 temp.track();
 
-function run(config: Config) {
-  console.log(config); // eslint-disable-line no-console
+function sequenceArray(count: number) {
+  return new Array(count).fill().map((_, i) => i + 1);
+}
+
+async function extractFonts(tmpDirPath, input) {
+  const mkvInfo = await exec(`mkvinfo ${input}`);
+  const fontCount = (mkvInfo.stdout.match(/Attached/g) || []).length;
+  if (fontCount === 0) { return; }
+  const fontsPath = path.join(tmpDirPath, 'fonts');
+  await pify(mkdirp)(fontsPath);
+  await exec(`mkvextract attachments ${input} ${sequenceArray(35).join(' ')}`, { cwd: fontsPath });
+}
+
+async function run(config: Config) {
+  const tmpDirPath = await pify(temp.mkdir)('shimakaze-js');
+  await extractFonts(tmpDirPath, config.input);
 }
 
 module.exports = function parseAndRun(config: InputConfig) {
-  run(parse(config));
+  return run(parse(config));
 };
